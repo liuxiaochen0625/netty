6e87b55e1cd2dfc3bfdcd057751e8430e409f63c
liuxiaoqiang <liuxiaoqiang@wdai.com>
2017-07-18 16:22:56 +0800

    添加log日志和测试类

---
+++ src/test/java/com/wdai/netty/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Weidai
+ * Copyright (c) 2017-2017 All Rights Reserved.
+ */
+package com.wdai.netty;
+
+/**
+ * @author reus
+ * @version $Id: Main.java, v 0.1 2017-07-18 reus Exp $
+ */
+public class Main {
+}
