9c16da2890a04d4ae1fdd70484cf88b0ec823b68
liuxiaoqiang <liuxiaoqiang@wdai.com>
2017-12-13 20:47:55 +0800

    KMP算法

---
+++ src/main/java/com/wdai/netty/util/KMP.java
@@ -0,0 +1,69 @@
+/**
+ * Weidai
+ * Copyright (C), 2011 - 2017, 微贷网.
+ */
+package com.wdai.netty.util;
+
+/**
+ * @author reus
+ * @version $Id: KMP.java, v 0.1 2017-12-13 reus Exp $
+ * KMP算法
+ */
+public class KMP {
+    /**
+     * 计算next数组
+     * @param compare
+     * @param next
+     */
+    private static void cal_next(String compare, int next[]) {
+        int len = compare.length();
+        next[0] = -1;//next[0]初始化为-1，-1表示不存在相同的最大前缀和最大后缀
+        int k = -1;//k初始化为-1
+        for (int q = 1; q <= len - 1; q++) {
+            while (k > -1 && compare.charAt(k + 1) != compare.charAt(q))//如果下一个不同，那么k就变成next[k]，注意next[k]是小于k的，无论k取任何值。
+            {
+                k = next[k];//往前回溯
+            }
+            if (compare.charAt(k + 1) == compare.charAt(q))//如果相同，k++
+            {
+                k = k + 1;
+            }
+            next[q] = k;//这个是把算的k的值（就是相同的最大前缀和最大后缀长）赋给next[q]
+        }
+    }
+
+    /**
+     * 查找子串的初始位置
+     * @param source
+     * @param compare
+     * @return
+     */
+    public static int kmp(String source, String compare) {
+        int slen = source.length();
+        int plen = compare.length();
+        int[] next = new int[plen];
+        cal_next(compare, next);//计算next数组
+        int k = -1;
+        for (int i = 0; i < slen; i++) {
+            while (k > -1 && compare.charAt(k + 1) != source.charAt(i))//ptr和str不匹配，且k>-1（表示ptr和str有部分匹配）
+                k = next[k];//往前回溯
+            if (compare.charAt(k + 1) == source.charAt(i))
+                k = k + 1;
+            if (k == plen - 1)//说明k移动到ptr的最末端
+            {
+                //cout << "在位置" << i-plen+1<< endl;
+                //k = -1;//重新初始化，寻找下一个
+                //i = i - plen + 1;//i定位到该位置，外层for循环i++可以继续找下一个（这里默认存在两个匹配字符串可以部分重叠），感谢评论中同学指出错误。
+                return i - plen + 1;//返回相应的位置
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String str = "bbc abcdab abcdabcdabde";
+        String ptr = "abcdabd";
+        int a = kmp(str, ptr);
+        System.out.println(a);
+    }
+}
